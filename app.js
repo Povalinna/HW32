825622afa42f5b24c48fa88b16b454085c3824f6
Povalinna <111307356+Povalinna@users.noreply.github.com>
2023-06-13 21:30:04 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,36 +1,17 @@
 
-function hour() {
+function currentClock() {
     let date = new Date();
     let hours = date.getHours() < 10 ? '0' + date.getHours() : date.getHours();
-    let boxHour = document.querySelector(`#timerH`);
-    boxHour.innerHTML = hours;
-    document.body.append(boxHour);
-}
-setInterval(hour, 3600000)
 
-function minute() {
-    let date = new Date();
     let minutes = date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes();
-    let boxMinute = document.querySelector(`#timerM`);
-    boxMinute.innerHTML = minutes;
-    document.body.append(boxMinute);
-}
-setInterval(minute, 60000)
 
-function second() {
-    let date = new Date();
     let seconds = date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds();
-    let boxSecond = document.querySelector(`#timerS`);
-    boxSecond.innerHTML = seconds;
-    document.body(boxSecond);
+
+    let currentTime = hours + `:` + minutes + `:` + seconds;
+    let boxTime = document.querySelector(`#time`);
+    boxTime.innerHTML = currentTime;
+    document.body.append(boxTime);
+
 }
-setInterval(second, 1000);
-
-
-
-
-
-
-hour(); minute(); second();
-
+setInterval(currentClock, 1000);
 
